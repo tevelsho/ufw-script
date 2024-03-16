8e17654da5bd1e1ef505716ba9b58cdb5abe38a7
tev <133293732+trevelling@users.noreply.github.com>
2024-03-16 19:59:03 +0800

    Fixed bug with code

--- firewall.py
+++ firewall.py
@@ -4,7 +4,7 @@ from systemd import journal
 import re
 import pyufw as ufw 
 
-j = jounral.Reader()
+j = journal.Reader()
 j.this_boot()
 j.log_level(journal.LOG_INFO)
 j.add_match(_SYSTEMD_UNIT="ssh.service")
@@ -13,9 +13,14 @@ ip_addresses = set()
 for entry in j: # Read every entry in the journal
         message = entry["MESSAGE"]
         if message.startswith("Failed"):
-                ip_addresses.update(re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', message>
+                ip_addresses.update(re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', message))
+
+ufw.default = "deny" # Set default ufw policy to deny
+ufw.enable = True # Enable ufw
 
 for ip in ip_addresses:
         print(ip)
         # Adding new ufw rule to deny from specific IP
         ufw.add("deny from " + ip)
+
+ufw.add("allow from any to any port 22") # Allow SSH traffic
